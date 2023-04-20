264b0203acca783fe3039b7615e701297cf8dc44
Orlundo Hubbard <weareprojectcode@gmail.com>
2023-04-20 08:35:01 -0500

    continuing with js

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
 const form = document.getElementById('story-form');
 const promptInput = document.getElementById('prompt');
 const storyContainer = document.getElementById('story-container');
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const prompt = promptInput.value;
+    generateStory(prompt);
+});
+
+async function generateStory(prompt) {
+    
+}
